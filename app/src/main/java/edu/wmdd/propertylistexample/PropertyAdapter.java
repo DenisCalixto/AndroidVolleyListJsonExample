a12e127defb456dd57b4e796ee2e73ec2626f01f
DenisCalixto <deniscalixto@gmail.com>
2019-10-03 08:13:04 -0700

    Initial commit

---
+++ app/src/main/java/edu/wmdd/propertylistexample/PropertyAdapter.java
@@ -0,0 +1,55 @@
+package edu.wmdd.propertylistexample;
+
+import android.content.Context;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PropertyAdapter extends RecyclerView.Adapter<PropertyAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Property> list;
+
+    public PropertyAdapter(Context context, List<Property> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.single_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Property movie = list.get(position);
+
+        holder.textTitle.setText(movie.getTitle());
+        holder.textRating.setText(String.valueOf(movie.getRating()));
+        holder.textYear.setText(String.valueOf(movie.getYear()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textTitle, textRating, textYear;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textTitle = itemView.findViewById(R.id.main_title);
+            textRating = itemView.findViewById(R.id.main_rating);
+            textYear = itemView.findViewById(R.id.main_year);
+        }
+    }
+
+}
